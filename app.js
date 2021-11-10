3d112c50f623f6dc70d95d744be7aca05a431e07
kelechi prosper <prosperkelechi50@gmail.com>
2021-11-10 08:57:46 +0100

    i

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ document.getElementById('loan-form').addEventListener('submit', calculateResults
 // calculate Results
 function calculateResults(e){
     console.log('calculating...');
-
+    // UI vars
+    
     e.preventDefault();
 }
