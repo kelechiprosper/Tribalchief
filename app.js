c9b253c9031fe94281308ea6e22c73b7109700c6
kelechi prosper <prosperkelechi50@gmail.com>
2021-11-10 09:03:44 +0100

    i

--- app.js
+++ app.js
@@ -5,6 +5,18 @@ document.getElementById('loan-form').addEventListener('submit', calculateResults
 function calculateResults(e){
     console.log('calculating...');
     // UI vars
+    const amount = document.getElementById('Amount');
+    const interest = document.getElementById('Interest');
+    const years = document.getElementById('Years');
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
+    
+    const principal = parseFloat (amount.value);
+    const calculatedInterest = parseFloat(interst.value)/ 100 / 12;
+    const calculatedPayment =parseFloat(years.value) * 12;
+
+    
     
     e.preventDefault();
 }
